438b1e61927c36f02cc849da6782aff1f2ee6aca
Michiel Sikma <michiel@sikma.org>
2024-06-15 21:22:30 +0200

    Add filetypes library

---
+++ src/lib/filetypes.fish
@@ -0,0 +1,13 @@
+# dada-fish <https://github.com/msikma/dada-fish>
+# © MIT license
+
+## Filetypes
+##
+## Contains information about filetypes.
+
+set -g _image_exts "gif" "png" "jpg" "jpeg" "jpf" "tga" "bmp" "tif" "tiff" "psd" "pdf" "eps" "ai" "webp" "avif" "pcx"
+set -g _interactive_exts "fla" "swf"
+set -g _video_exts "mp4" "m4p" "m4v" "mkv" "avi" "webm" "mpg" "mts" "m2ts" "ts" "yuv" "rm" "rmvb" "asf" "amv" "mp2" "mpeg" "mpe" "mpv" "m2v" "ogm" "3gp" "3g2" "wmv" "mov" "qt" "flv" "f4v" "f4p" "f4a" "f4b"
+set -g _audio_exts "mp3" "ac3" "amr" "mp1" "spx" "wma" "aac" "mpc" "vqf" "ots" "swa" "vox" "voc" "ogg" "flac" "8svx" "16svx" "aiff" "aif" "aifc" "au" "bwf" "cdda" "dsf" "dff" "raw" "wav" "ra" "rm" "ape" "brstm" "ast" "aw" "psf"
+
+set _media_exts $_image_exts $_interactive_exts $_video_exts $_audio_exts
