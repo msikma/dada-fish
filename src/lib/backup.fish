d12e3ae35fb893f0338fb1a2c7208810bd334756
Michiel Sikma <michiel@sikma.org>
2024-06-15 21:48:35 +0200

    Add function for checking required backup dirs

--- src/lib/backup.fish
+++ src/lib/backup.fish
@@ -30,3 +30,22 @@ function _check_rsync_version
 
   return 0
 end
+
+## Checks that a number of directories exists and can be backed up or written to.
+function _require_needed_dirs --argument-names script_name dir_type
+  set required_dirs $argv[3..-1]
+  set dir_count (count $required_dirs)
+  if [ $dir_count -eq 0 ]
+    echo $script_name": error: called _check_needed_dirs with no directory arguments"
+    return 1
+  end
+  for n in (seq 1 $dir_count)
+    set required_dir $required_dirs[$n]
+    if [ ! -d $required_dir ]
+      echo $script_name": error: can't access $dir_type directory: "$required_dir
+      return 1
+    end
+  end
+
+  return 0
+end
