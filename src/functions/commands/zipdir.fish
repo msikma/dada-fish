3b49074d38306016f2546df3c05945650786a35d
Michiel Sikma <michiel@sikma.org>
2024-06-22 13:48:44 +0200

    Default to not including the dir itself

--- src/functions/commands/zipdir.fish
+++ src/functions/commands/zipdir.fish
@@ -7,6 +7,9 @@ set -g _default_level '6'
 function zipdir --description "Zips a directory (also: zipdir{n})" --argument-names dirpath level
   ! _require_cmd "gstat"; and return 1
   ! _require_cmd "gls"; and return 1
+  ! _require_cmd "zip"; and return 1
+
+  set _include_dir 0
 
   set usage "usage: zipdir0..9 [--help] dir"
 
@@ -37,19 +40,27 @@ function zipdir --description "Zips a directory (also: zipdir{n})" --argument-na
     return 1
   end
 
-  set source_parent (realpath "$source_dir/..")
   set target_basename (basename $dirpath)
   set target_name "$target_basename.zip"
   set target_path "$here"/"$target_name"
+  if [ "$_include_dir" -eq 1 ]
+    set source_zip_reldir (realpath "$source_dir/..")
+    set source_files "./$target_basename"
+  else
+    set source_zip_reldir (realpath "$source_dir")
+    set source_files "."
+  end
   
   echo (set_color yellow)"Zipping to: "(set_color cyan)$target_name(set_color normal)
   echo
-  pushd "$source_parent"
-  time zip -$level -db -Xovr "$target_path" "./$target_basename" -x "*.DS_Store" # | grep --color=never "total bytes"
-  # Set the access time to the source directory's.
-  touch -r "./$target_basename" "$target_path"
-  echo
+  echo $target_path
+
+  pushd "$source_zip_reldir"
+  zip -$level -db -Xovr "$target_path" "$source_files" -x "*.DS_Store"
+  #touch -r "$source_files" "$target_path"  note: not needed due to -o
   popd
+  
+  echo
 
   set result_path "./$target_name"
   set result_filesize (gls -lh "$target_path" | awk '{print $5}')
@@ -68,6 +79,7 @@ for n in (seq 0 9)
   eval "function zipdir$n --description \"zipdir at compression level $n\" --argument-names dirpath; zipdir \"\$dirpath\" '$n'; end"
 end
 
+_register_dependency "brew" "zip" "zip"
 _register_dependency "brew" "coreutils" "gstat"
 _register_dependency "brew" "coreutils" "gls"
 _register_command archiving "zipdir" "dir level"
