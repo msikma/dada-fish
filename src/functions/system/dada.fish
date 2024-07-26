221574a71d532f398857c47fc62c4a1498efd7ee
Michiel Sikma <michiel@sikma.org>
2024-07-26 18:29:20 +0900

    Add dada_install function (placeholder)

--- src/functions/system/dada.fish
+++ src/functions/system/dada.fish
@@ -5,4 +5,9 @@ function dada_reload --description "Reloads the system"
   source "$DADA_FISH/main.fish"
 end
 
+function dada_install --description "Runs installation script"
+  echo "Placeholder."
+end
+
 _register_command system "dada_reload"
+_register_command system "dada_install"
