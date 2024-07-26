30c2ef7a0294b0d2266786a36911b2311ca04bd9
Michiel Sikma <michiel@sikma.org>
2024-07-26 18:29:30 +0900

    Require pygments

--- src/functions/commands/headers.fish
+++ src/functions/commands/headers.fish
@@ -2,7 +2,10 @@
 # © MIT license
 
 function headers --argument-names url --description "Displays response headers for URL"
+  ! _require_cmd "pygmentize"; and return 1
+
   curl -kLsD- -o /dev/null "$url" | pygmentize -l http
 end
 
 _register_command script "headers" "url"
+_register_dependency "brew" "pygments" "pygmentize"
