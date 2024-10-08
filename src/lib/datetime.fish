d138af8e573286cec89efd06802648d96b428ff4
Michiel Sikma <michiel@sikma.org>
2024-10-08 12:28:11 +0200

    Add _timestamp_tz, and add years to relative timestamp

--- src/lib/datetime.fish
+++ src/lib/datetime.fish
@@ -4,15 +4,23 @@
 ## Returns an ISO 8601 timestamp string.
 function _timestamp_iso8601 --argument-names timestamp
   if [ -z "$timestamp" ]
-    gdate -u +'%Y-%m-%d %H:%M:%S.%3N'
-  else
-    gdate -u -d "$timestamp" +"%Y-%m-%d %H:%M:%S.%3N"
+    set timestamp "@"(gdate -u +"%s")
   end
+  gdate -u -d "$timestamp" +"%Y-%m-%d %H:%M:%S.%3N"
 end
 
-## Returns a relative time from a given Unix time value.
+## Returns a timestamp string with timezone.
+function _timestamp_tz --argument-names timestamp
+  if [ -z "$timestamp" ]
+    set timestamp "@"(gdate -u +"%s")
+  end
+  gdate -d "$timestamp" +"%Y-%m-%d %H:%M:%S %z"
+end
+
+## Returns a relative time from a given timestamp string.
 function _relative_timestamp --argument-names timestamp
   set now (date +%s)
+  set timestamp (gdate -u -d "$timestamp" +"%s")
   set diff (math "$now - $timestamp")
 
   if [ $diff -eq 0 ]
@@ -46,11 +54,14 @@ function _relative_timestamp --argument-names timestamp
   else if [ $diff -lt 2592000 ]
     set unit "week"
     set value (math "$diff / 604800")
-  else
+  else if [ $diff -lt 31536000 ]
     set unit "month"
     set value (math "$diff / 2592000")
+  else
+    set unit "year"
+    set value (math "$diff / 31536000")
   end
-
+  
   set value (math "round $value")
   if [ $value -ne 1 ]
     set unit "$unit""s"
