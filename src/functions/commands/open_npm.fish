a82a265d5720c518d76846d88c7821c2a1d89f4b
Michiel Sikma <michiel@sikma.org>
2024-10-08 12:27:46 +0200

    Use jq instead of node

--- src/functions/commands/open_npm.fish
+++ src/functions/commands/open_npm.fish
@@ -3,17 +3,19 @@
 
 function open_npm --description "Opens this npm pkg in the browser"
   ! _require_cmd "open" "open_npm: error: cannot open URLs on this system"; and return 1
+  ! _require_cmd "jq"; and return 1
 
-  set pkgname (node -e "const a = require('./package.json'); a.name && console.log(a.name);" 2> /dev/null)
+  set pkg_name (jq -r ".name" < "./package.json" 2> /dev/null)
 
-  if [ -z "$pkgname" ]
+  if [ -z "$pkg_name" -o "$pkg_name" = "null" ]
     echo "open_npm: error: not an npm project"
     return 1
   end
 
-  set pkgurl "https://www.npmjs.com/package/$pkgname"
+  set pkgurl "https://www.npmjs.com/package/$pkg_name"
   echo (set_color yellow)"Opening npm package url: "(set_color reset)(set_color -u)"$pkgurl"
   open "$pkgurl"
 end
 
 _register_command pkg "open_npm"
+_register_dependency "brew" "jq" "jq"
--- src/functions/commands/open_repo.fish
+++ src/functions/commands/open_repo.fish
@@ -3,13 +3,14 @@
 
 function open_repo --description "Opens this repo in the browser"
   ! _require_cmd "open" "open_npm: error: cannot open URLs on this system"; and return 1
+  ! _require_cmd "jq"; and return 1
 
   # Note: only supports NodeJS packages.
   if [ -e "./package.json" ]
-    set url (node -e "const a = require('./package.json'); a.homepage && console.log(a.homepage);" 2> /dev/null)
+    set url (jq -r ".homepage" < "./package.json" 2> /dev/null)
   end
 
-  if [ -z "$url" ]
+  if [ -z "$url" -o "$url" = "null" ]
     echo "open_repo: error: could not find repository URL"
     return 1
   end
@@ -19,3 +20,4 @@ function open_repo --description "Opens this repo in the browser"
 end
 
 _register_command pkg "open_repo"
+_register_dependency "brew" "jq" "jq"
