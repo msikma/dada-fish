6ab7c92080b20ac2e34b3743f4017d22b2ba832f
Michiel Sikma <michiel@sikma.org>
2024-11-09 16:49:04 +0100

    Update for new structure

--- src/env/plugins/dada-icons.fish
+++ src/env/plugins/dada-icons.fish
@@ -1,11 +1,11 @@
 # dada-fish <https://github.com/msikma/dada-fish>
 # © MIT license
 
-set -l _icon_parent_dirs "Code" "Code archive" "Design"
+set -l _icon_parent_dirs "Design"
 
 for dir in $_icon_parent_dirs
-  set dada_folder_icon_path ~/"Files/Work/$dir/dada-folder-icons/icns"
-  set dada_icon_path ~/"Files/Work/$dir/dada-icons/icns"
+  set dada_folder_icon_path ~/"Files/Projects/$dir/dada-folder-icons/icns"
+  set dada_icon_path ~/"Files/Projects/$dir/dada-icons/icns"
 
   if [ -d $dada_folder_icon_path ]
     set -g DADA_FOLDER_ICON_DIR $dada_folder_icon_path
