ffae6b5156c4401d6d75589e1bf70676d588d3cf
Michiel Sikma <michiel@sikma.org>
2024-11-22 22:38:21 +0100

    Add XDG variables

---
+++ src/env/plugins/xdg.fish
@@ -0,0 +1,7 @@
+# dada-fish <https://github.com/msikma/dada-fish>
+# © MIT license
+
+# Normally: /Users/msikma/Library/Preferences
+set -gx XDG_CONFIG_HOME ~/.config
+# Normally: /Users/msikma/Library/Caches
+set -gx XDG_CACHE_HOME ~/.cache
