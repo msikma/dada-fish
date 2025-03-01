cae3bb94f3043d12efea14f33c8792e09913f1fd
Michiel Sikma <michiel@sikma.org>
2025-03-01 12:59:28 +0100

    Really we just need this

--- src/backup/plugins/firefox.fish
+++ src/backup/plugins/firefox.fish
@@ -1,4 +1,4 @@
 # dada-fish <https://github.com/msikma/dada-fish>
 # © MIT license
 
-_register_backup_dir "userdata" "Firefox profile data" ~/"Documents/Firefox profile"
+_register_backup_dir "userdata" "Firefox profile data" ~/"Documents/Firefox profile/sessionstore-backups"
