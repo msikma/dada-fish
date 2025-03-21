98a0a22c470a494112654b6cf9d082286059b251
Michiel Sikma <michiel@sikma.org>
2025-03-21 11:51:08 +0100

    Add Intel path

--- src/env/plugins/homebrew.fish
+++ src/env/plugins/homebrew.fish
@@ -2,5 +2,10 @@
 # © MIT license
 
 if [ -d /opt/homebrew/bin ]
+  # Apple Silicon
   set PATH /opt/homebrew/bin $PATH
+end
+if [ -d /usr/local/bin ]
+  # Intel
+  set PATH /usr/local/bin $PATH
 end
