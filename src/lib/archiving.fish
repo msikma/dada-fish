528f4e9965766415e74f7adf0d5e3fbd45e9e1f4
Michiel Sikma <michiel@sikma.org>
2025-03-26 13:16:31 +0100

    Improve info.json handling for playlists, and add webloc function

--- src/lib/archiving.fish
+++ src/lib/archiving.fish
@@ -1,6 +1,19 @@
 # dada-fish <https://github.com/msikma/dada-fish>
 # © MIT license
 
+# Writes a .webloc file.
+function _write_webloc --argument-names url target_basename
+  set content "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
+<plist version=\"1.0\">
+<dict>
+	<key>URL</key>
+	<string>$url</string>
+</dict>
+</plist>"
+  echo "$content" > "$target_basename.webloc"
+end
+
 ## Returns a filename (or directory name) suggestion for a yt-dlp info.json file.
 function _ytdlp_get_name --argument-names info_json_fn
   ! _require_cmd "jq"; and return 1
@@ -18,12 +31,26 @@ function _ytdlp_find_info_json --argument-names dir
   if test (count $files) -le 1
     echo "$files[1]"
   else
-    # We've found multiple files. This is usually the case for SOOP items,
-    # which are actually playlists divided into multiple items (which get merged).
-    # Since the playlist .info.json files will have "(part 1)" etc. in it,
-    # and a longer id, we can sort by length and get the first one.
+    # If we've found multiple files, that usually means this is a playlist.
+    # Some .info.json files will have type "video" and one will have "playlist".
+    # Find the "playlist" item and use that one.
+    set playlist_files
+    for file in $files
+      if test (cat "$file" | jq -r "._type") = "playlist"
+        set -a playlist_files $file
+      end
+    end
+    # Just in case we still have multiple files here, get the shortest one.
+    # Or, if we don't have any playlist files at all, pick the shortest from the others.
+    # This is because the "main" item will likely be the shortest, as others
+    # will have (part 1) etc. in their filename.
+    if test (count $playlist_files) -gt 0
+      set relevant_files $playlist_files
+    else
+      set relevant_files $files
+    end
     set sorted (
-      for file in $files
+      for file in $relevant_files
         echo $file | awk '{ print length($0), $0 }'
       end | sort -n | cut -d' ' -f2-
     )
