f8a79e12bb1ff77239aff2bad5cac3ea1a7d368b
Michiel Sikma <michiel@sikma.org>
2025-04-25 17:40:12 +0200

    Simplify yt_audio

--- src/functions/commands/yt_audio.fish
+++ src/functions/commands/yt_audio.fish
@@ -1,34 +1,36 @@
 # dada-fish <https://github.com/msikma/dada-fish>
 # © MIT license
 
+function _yt_audio_usage
+  echo "usage: yt_audio [-h|--help] [-na] [-nc] URL…"
+  echo ""
+  echo "Arguments:"
+  echo "  --help, -h    prints usage information"
+  echo "  -na           no download archive check (redownload)"
+  echo "  -nc           no browser cookies"
+end
+
 function yt_audio --description "Archives audio from various sites"
-  ! _require_cmd "yt-dlp"; and return 1
-
-  # If the first argument is not a URL, assume it's the audio format.
-  # Default to 'best' audio (variable output extension).
-  if string match -q "http*" $argv[1]
-    set audio_format "best"
-    set urls $argv
-  else
-    set audio_format $argv[1]
-    set urls $argv[2..-1]
+  set yt_archive_file "$DADA_CACHE/yt_audio_archive.txt"
+  set arg_dl_archive "--download-archive" "$yt_archive_file"
+  if contains -- "-na" $argv
+    set arg_dl_archive
   end
-
-  # Check whether this is a playlist or not.
-  # If so, include the playlist_index variable in the filename.
-  set tpl_playlist "%(playlist_index)s - %(title)s [%(id)s].%(ext)s"
-  set tpl_single "%(autonumber)02d %(title)s (%(upload_date>%Y-%m-%d)s) [%(id)s].%(ext)s"
-  for n in (seq (count $urls))
-    set url $urls[$n]
-    if begin string match -qr -- ".+?playlist\?list.+?" $url; \
-      or string match -qr -- ".+?&list=.+?" $url; end
-      set tpl $tpl_playlist
-      echo (set_color magenta)"Downloading in playlist mode"(set_color normal)
-    else
-      set tpl $tpl_single
+  set arg_cookies "--cookies-from-browser" "firefox"
+  if contains -- "-nc" $argv
+    set arg_cookies
+  end
+  for n in (seq (count $argv))
+    set arg $argv[$n]
+    if string match -q -- "-*" "$arg"
+      continue
     end
-    
-    yt-dlp -i --format "bestaudio" -x --convert-thumbnail jpg --add-metadata --embed-metadata --embed-thumbnail --autonumber-start "$n" --audio-format $audio_format -o $tpl $url
+    yt-dlp --ignore-errors --format "bestaudio" --extract-audio \
+      --convert-thumbnail jpg --add-metadata --embed-metadata \
+      --embed-thumbnail --autonumber-start "$n" --audio-format "best" \
+      $arg_dl_archive \
+      $arg_cookies \
+      "$arg"
   end
 end
 
