a5b6bd7240485ea83e18a79086b354172499a3c8
Michiel Sikma <michiel@sikma.org>
2025-07-13 17:01:11 +0200

    Add script for tracking down duplicates in archived Youtube tracks

---
+++ src/functions/bin/find_yt_dupes.fish
@@ -0,0 +1,33 @@
+#!/usr/bin/env fish
+
+set USAGE 'usage: find_yt_dupes NEW_DIR OLD_DIR'
+
+function main --argument-names new_dir old_dir
+  if not test -d "$new_dir"
+    echo "$USAGE"
+    return 1
+  end
+  if not test -d "$old_dir"
+    echo "$USAGE"
+    return 1
+  end
+  for file in "$new_dir"/*
+    if not test -f "$file"
+      continue
+    end
+
+    set youtube_id (string match -r '\[([^][]{11})\]' "$file" | tail -n 1)
+
+    if test -n "$youtube_id"
+      set found_in_archive (find "$old_dir" -name '*\['"$youtube_id"'\]*' -print -quit)
+      if test -n "$found_in_archive"
+        echo "#" "$found_in_archive"
+        echo "rm" '"'"$file"'"'
+      end
+    end
+  end
+end
+
+main $argv
+
+#_dada_fish _register_bin regular "find_yt_dupes" "NEW_DIR OLD_DIR" "find_yt_dupes.fish" "Finds Youtube archive duplicates"
